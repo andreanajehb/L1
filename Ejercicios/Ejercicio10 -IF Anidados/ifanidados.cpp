642725c695119e53effcc6251f07537b9b7111df
Andrea Hernandez <you@example.com>
2020-07-19 22:28:58 -0600

    agregando IF anidados

---
+++ Ejercicios/Ejercicio10 -IF Anidados/ifanidados.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int a = 1000;
+	int b = 5000;
+	int c = 7000;
+	
+	if (a > b) {
+		if (a > c) {
+			cout << "A es el mayor";
+		} else {
+			cout << "C es el mayor";
+		}
+	} else {
+		if (b > c) {
+			cout << "B es el mayor";
+		} else {
+			cout << "C es el mayor";
+		}
+	}
+		
+	return 0;
+}
