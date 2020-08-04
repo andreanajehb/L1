9f56ce9e06a651365131dd6d1d39c4bd0bde6d18
Andrea Hernandez <you@example.com>
2020-08-03 23:40:31 -0600

    Tarea #6

--- Ejercicios/Ejercicio45 - Punto de Venta BEBIDAS CALIENTES, BEBIDAS FRIAS Y REPOSTERIA/factura.cpp
+++ Ejercicios/Ejercicio45 - Punto de Venta BEBIDAS CALIENTES, BEBIDAS FRIAS Y REPOSTERIA/factura.cpp
@@ -15,21 +15,21 @@ void agregarProducto(string descripcion, int cantidad, double precio)
 void imprimirFactura()
 {
     system("cls");
-    cout<< "*******" << endl;
+    cout<< "****" << endl;
     cout<< "FACTURA" << endl;
-    cout<< "*******" << endl;
+    cout<< "****" << endl;
     cout<< endl;
 
-    cout<< "Productos:" << endl;
+    cout<< "Productos: " << endl;
     cout<< listaProductos;
 
     calculoImp = subtotal * 0.15;
     
     cout<< "-------------------------------------";
     cout<< endl;
-    cout<< "Subtotal:" << subtotal;
+    cout<< "Subtotal: " << subtotal;
     cout<< endl;
-    cout<< "Impuesto incluido: " << calculoImp;
+    cout<< "Impuesto ya incluido: " << calculoImp;
     cout<< endl;
     cout<< "Total a pagar: " << subtotal + calculoImp ;
     cout<< endl;
