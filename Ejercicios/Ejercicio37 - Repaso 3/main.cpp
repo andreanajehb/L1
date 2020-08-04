e9ecfe571be77305e382bc0778ea7b8c82bb5a93
Andrea Hernandez <you@example.com>
2020-08-03 22:58:07 -0600

    Repaso #3

---
+++ Ejercicios/Ejercicio37 - Repaso 3/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    int a = 0;
+    int b = 10;
+    int c = 4;
+
+   // if (a > b && a > c){
+  //     cout<< "a es mayor que b o mayor que c";
+   // }
+    
+   // while(a > b){
+   //     a--;
+
+   //     if(a == 20){
+   //         break;
+   //     }
+   // }
+
+   while (true)
+   {
+       while (true)
+        {
+          if (a == 10)
+            {
+             break;
+            }
+        }
+        a++;
+
+        if(a == 1)
+        {
+         break;
+        }
+    }
+   
+
+
+
+    return 0;
+}
